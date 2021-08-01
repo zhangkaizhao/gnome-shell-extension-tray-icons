b7b23157fd039bdde14feae43b00d0b1823b852c
Himura Kazuto <glagol15@gmail.com>
2021-08-01 22:56:05 +0300

    Apply suggestions from code review

--- extension.js
+++ extension.js
@@ -7,7 +7,7 @@ const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 const System = imports.system;
 
-const TRAY_ICON_SIZE = 18;
+const TRAY_ICON_SIZE = 14;
 
 let tray = null;
 let icons = [];
